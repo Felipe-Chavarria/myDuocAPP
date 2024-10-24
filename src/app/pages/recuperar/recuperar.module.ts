fef0889e2ed6a961bf3b7bdf413a49471b1ef854
CjhCris <cmduartesaez@gmail.com>
2024-10-24 15:11:13 -0300

    recuperar contraseña
    ayuda

--- src/app/pages/recuperar/recuperar.module.ts
+++ src/app/pages/recuperar/recuperar.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
+import { HttpClientModule } from '@angular/common/http'; // Importa HttpClientModule
 
 import { RecuperarPageRoutingModule } from './recuperar-routing.module';
-
 import { RecuperarPage } from './recuperar.page';
 
 @NgModule({
@@ -13,6 +12,7 @@ import { RecuperarPage } from './recuperar.page';
     CommonModule,
     FormsModule,
     IonicModule,
+    HttpClientModule, // Asegúrate de incluir HttpClientModule
     RecuperarPageRoutingModule
   ],
   declarations: [RecuperarPage]
--- src/app/pages/recuperar/recuperar.page.ts
+++ src/app/pages/recuperar/recuperar.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { RecuperarService } from './recuperar.service'; // Ajusta la ruta según sea necesario
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-recuperar',
@@ -8,12 +10,36 @@ import { Component, OnInit } from '@angular/core';
 export class RecuperarPage {
   correo: string = '';
 
+  constructor(private recuperarService: RecuperarService, private alertController: AlertController) {}
+
+  async mostrarAlerta(titulo: string, mensaje: string) {
+    const alert = await this.alertController.create({
+      header: titulo,
+      message: mensaje,
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
+
   recuperar() {
     if (this.correo.length >= 10) {
-      // Lógica para el envío del correo de recuperación
-      console.log(`Se ha enviado un correo de recuperación a: ${this.correo}`);
+      this.recuperarService.recuperarContraseña(this.correo).subscribe(
+        response => {
+          this.mostrarAlerta('Éxito', response.message); // Usa el mensaje de la respuesta
+        },
+        error => {
+          console.error('Error al enviar el correo', error);
+          
+          let mensajeError = 'No se pudo enviar el correo, por favor intenta nuevamente.';
+          if (error.error && error.error.message) {
+            mensajeError = error.error.message; // Usa el mensaje de error de la respuesta
+          }
+          this.mostrarAlerta('Error', mensajeError);
+        }
+      );
     } else {
       console.log('Correo inválido');
+      this.mostrarAlerta('Error', 'Correo inválido');
     }
   }
 }
