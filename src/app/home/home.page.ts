19791e5bcb91f7c1f2f00b63d2bff87e8ceea5c0
Camilo <xamilo447@gmail.com>
2024-11-27 22:29:31 -0300

    Dark Mode

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,34 +7,34 @@ import { Router } from '@angular/router';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage  {
-  
-  public usuario: string ;
+export class HomePage implements OnInit {
+  public usuario: string = '';
   public perfil: any;
+  public currentTheme: 'light' | 'dark' = 'light';
 
-  constructor(private NavCtrl: NavController, private router: Router) {
-    this.usuario = '';
-    this.perfil = '';
-  }
+  constructor(private NavCtrl: NavController, private router: Router) {}
 
   ngOnInit() {
-    const storedUser =  localStorage.getItem('usuario');
-    if(storedUser){
-      this.usuario = storedUser;
-    }else{
-      this.usuario = 'Usuario';
-    }
-    const storedPerfil = localStorage.getItem('perfil');
-    this.perfil = storedPerfil;
+    const storedUser = localStorage.getItem('usuario');
+    this.usuario = storedUser ? storedUser : 'Usuario';
 
+    this.perfil = localStorage.getItem('perfil');
+
+    const storedTheme = localStorage.getItem('theme') || 'light';
+    this.currentTheme = storedTheme as 'light' | 'dark';
+    document.body.setAttribute('color-theme', this.currentTheme);
   }
 
-  cerrarSesion(){
+  cerrarSesion() {
     localStorage.removeItem('token');
     localStorage.removeItem('perfil');
     localStorage.removeItem('usuario');
-    const user = localStorage.getItem('usuario');
-    console.log(user);
     this.router.navigate(['/login']);
+  }
+
+  toggleTheme() {
+    this.currentTheme = this.currentTheme === 'dark' ? 'light' : 'dark';
+    document.body.setAttribute('color-theme', this.currentTheme);
+    localStorage.setItem('theme', this.currentTheme);
   }
 }
