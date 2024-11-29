fedc6e0bd96b93c033fc313e05f128f4c6b10f2e
Martin Alonso Reyes Benavides <132596883+FalkFlow@users.noreply.github.com>
2024-11-29 16:17:42 -0300

    reinstalacion extenciones y paquetes android

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { AuthService } from '../providers/auth.service';
 
